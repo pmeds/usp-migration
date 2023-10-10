6d6b17313f2db3caa85018790379cbd385e4400c
pmeds <109703314+pmeds@users.noreply.github.com>
2023-10-10 12:50:28 -0600

    Update missing-files-comp.py

--- missing-files-comp.py
+++ missing-files-comp.py
@@ -3,7 +3,9 @@ import json
 def read_json(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
-    return {record['Name']: record for record in data}
+    records = {record['Name']: record for record in data}
+    print(f"Loaded {len(records)} unique records from {filename}.")
+    return records
 
 def write_json(filename, data):
     with open(filename, 'w') as file:
@@ -17,6 +19,8 @@ source_data = read_json(source_file)
 target_data = read_json(target_file)
 
 missing_keys = set(source_data.keys()) - set(target_data.keys())
+
+print(f"Number of missing keys: {len(missing_keys)}")
 
 missing_data = {key: source_data[key] for key in missing_keys}
 
