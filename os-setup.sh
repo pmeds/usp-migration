b1fc8416747df9b4f37e175ff2a33796a535f7e5
pmeds <109703314+pmeds@users.noreply.github.com>
2023-11-02 14:42:44 -0600

    Update os-setup.sh

--- os-setup.sh
+++ os-setup.sh
@@ -30,13 +30,13 @@ sh update_sshd.sh
 
 #Set hostname
 echo "Set hostname"
-hostnamectl set-hostname vm1.webmd-usc-poc.net
+hostnamectl set-hostname vm1.test-usp-poc.net
 
 # DEFAULT IP FOR HOSTNAME
 LOCALIP="127.0.0.1"
 
 # Hostname to add/remove.
-HOSTNAME="vm1.webmd-usc-poc.net"
+HOSTNAME="vm1.test-usp-poc.net"
 
 IP="$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 | sed -n 1p)"
 
